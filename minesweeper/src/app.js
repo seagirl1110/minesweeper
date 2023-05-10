6b441b0ea8ea78353396dff5a63767779b48e7e3
Marina Elizarova <seagirl1110@gmail.com>
2023-05-10 20:43:07 +0300

    feat: add mine-field

--- minesweeper/src/app.js
+++ minesweeper/src/app.js
@@ -1,1 +1,20 @@
 import './style.scss';
+
+const body = document.body;
+const field = document.createElement('div');
+field.classList.add('field');
+body.appendChild(field);
+
+const items = [];
+for (let i = 0; i < 100; i += 1) {
+  const item = document.createElement('div');
+  item.classList.add('field__item');
+  item.setAttribute('num', i);
+  item.addEventListener('click', (e) => {
+    item.classList.add('field__item--open');
+    item.innerText = i;
+    console.log(e.target.getAttribute('num'));
+  })
+  items.push(item);
+}
+field.append(...items);
