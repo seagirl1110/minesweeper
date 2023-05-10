d2a235e5902fe0bc76d3538ca91a4dc82af45ffe
Marina Elizarova <seagirl1110@gmail.com>
2023-05-10 21:29:40 +0300

    feat: add mine

--- minesweeper/src/app.js
+++ minesweeper/src/app.js
@@ -5,16 +5,31 @@ const field = document.createElement('div');
 field.classList.add('field');
 body.appendChild(field);
 
+const mines = [];
+while (mines.length < 10) {
+  const mine = Math.floor(Math.random() * 100);
+  if (!mines.includes(mine)) {
+    mines.push(mine);
+  }
+}
+
+console.log(mines);
+
 const items = [];
 for (let i = 0; i < 100; i += 1) {
   const item = document.createElement('div');
   item.classList.add('field__item');
   item.setAttribute('num', i);
+  let value = '';
+  if (mines.includes(i)) {
+    value = 'mine';
+  } else {
+    value = i;
+  }
+  item.setAttribute('value', value);
   item.addEventListener('click', (e) => {
     item.classList.add('field__item--open');
-    item.innerText = i;
-    console.log(e.target.getAttribute('num'));
-  })
+  });
   items.push(item);
 }
 field.append(...items);
